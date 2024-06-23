e9d5a4c7cc4286da96adb59659fb3f0bb6537cb8
sinchana c poojary <sinchanac91@gmail.com>
2024-06-23 19:17:40 +0530

    basic frontend was made

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React,{useState, useEffect } from 'react'  ;
+import AddTodo from './components/AddTodo';
+import TodoList from './components/TodoList';
+import './App.css';
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  useEffect(()=>{
+    fetchTodos();
+  },[]);
+  const fetchTodos = async () => {
+    const res = await fetch('http://localhost:5000/api/todos');
+    const data = await res.json();
+    setTodos(data);
+  };
 
-function App() {
-  const [count, setCount] = useState(0)
+  const addTodo = async (todo) => {
+    const res = await fetch('http://localhost:5000/api/todos',{
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(todo)
+    });
+    const data = await res.json();
+    setTodos([...todos,data]);
+  };
 
-  return (
-    <>
+  const toggleComplete = async (id) => {
+    const todoToToggle=todos.find(todo=>todo._id===id);
+    const updatedTodo={...todoToToggle,completed:!todoToToggle.completed};
+    const res = await fetch(`http://localhost:5000/api/todos/${id}`,{
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTodo)
+    });
+    const data = await res.json();
+    setTodos(todos.map(todo=>todo._id===id ? {...todo,completed:data.completed} : todo));
+  };
+
+    const deleteTodo=async (id) => {
+      const res = await fetch(`http://localhost:5000/api/todos/${id}`,{
+        method: 'DELETE'
+      });
+      setTodos(todos.filter(todo=>todo._id!==id));
+    };
+
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Todo List</h1>
+        <AddTodo addTodo={addTodo}/>
+        <TodoList todos={todos} toggleComplete={toggleComplete} deleteTodo={deleteTodo}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    );
+  };
 
-export default App
+  export default App;
